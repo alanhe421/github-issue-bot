f23e7431b2ffdf54c086d94ae5c052de290a239a
alanhe <qianghe421@gmail.com>
2022-05-03 22:39:19 +0800

    add repo path check

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create();
  * 用户
  */
 class User {
-  _repos = ['alanhg/others-note', 'alanhg/coding-note'];
+  _repos = [];
 
   constructor() {
   }
@@ -36,6 +36,7 @@ class User {
 
 const user = new User();
 
+
 const bot = new TelegramBot(token, {
   polling: true
 });
@@ -43,7 +44,7 @@ const bot = new TelegramBot(token, {
 
 bot.onText(/\/help$/, (msg, match) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'you can search your repos by keyword. \nFirstly, add some repos');
+  bot.sendMessage(chatId, 'you can search your repos by keyword. \nFirstly, /repo-add');
 });
 
 
@@ -63,8 +64,12 @@ bot.onText(/\/repo-add$/, async (msg, match) => {
     }
   });
   bot.onReplyToMessage(sended.chat.id, sended.message_id, (msg) => {
-    user.addRepo(msg.text);
-    bot.sendMessage(sended.chat.id, `repo added\nThe following repos is ${user.reposStr}`);
+    if (repoPathIsValid(msg.text)) {
+      user.addRepo(msg.text.trim());
+      bot.sendMessage(sended.chat.id, `repo added\nThe following repos is ${user.reposStr}`);
+    } else {
+      bot.sendMessage(sended.chat.id, `repo name invalid, send repo path like yagop/node-telegram-bot-api`);
+    }
   })
 });
 
@@ -86,10 +91,19 @@ bot.on('message', async (msg) => {
 
   const issues = await user.searchIssues(msg.text);
   if (issues.length) {
-    bot.editMessageText(issues.map(item => item.title + item.html_url).join('\n'), {
+    bot.editMessageText(issues.map((item, index) => `${index + 1}.${item.title}：${item.html_url}`).join('\n\n'), {
       message_id: sended.message_id, chat_id: chatId
     });
   } else {
     bot.editMessageText('No results matched your search.', {message_id: sended.message_id, chat_id: chatId});
   }
 });
+
+
+function repoPathIsValid(repoPath) {
+  repoPath = repoPath.trim();
+  if (repoPath) {
+    return repoPath.match(/^[^/]+\/[^/]+$/)
+  }
+  return false;
+}
